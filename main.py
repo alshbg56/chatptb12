d76b61ad2b836d6f5345027c8bd9edc44aa43f24
alshbg56 <124146631+alshbg56@users.noreply.github.com>
2023-04-15 18:18:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import os, requests
+from pyrogram import Client, filters, enums
+
+api_id = 24726477
+api_hash = "ba993a0f5b0bccfbf2a25d061826eeed"
+bot_token = os.environ['bot_token']
+
+app = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
+@app.on_message(filters.command(['start']))
+def hi(app, msg):
+  app.send_message(msg.chat.id, "مرحبا بك في بوت ذكاء الاصطناعي")
+  return
+
+@app.on_message(filters.text)
+def replytomsg(app, msg):
+  app.send_chat_action(msg.chat.id, enums.ChatAction.TYPING)
+  msgchat = requests.get("https://alsh-bg.ml/openai/x.php?key="+os.environ['api_key']+"&text=" + msg.text).json()['msg']
+  app.send_message(msg.chat.id, msgchat)
+  return
+  
+app.run()
